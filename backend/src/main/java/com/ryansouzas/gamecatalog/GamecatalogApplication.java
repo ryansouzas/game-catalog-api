8385c134b8f2074cd3702ffc5167588a41e6b028
Ryan Souza <ryanssebadelhe@gmail.com>
2026-07-09 12:10:44 -0300

    Project Created

---
+++ backend/src/main/java/com/ryansouzas/gamecatalog/GamecatalogApplication.java
@@ -0,0 +1,13 @@
+package com.ryansouzas.gamecatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamecatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamecatalogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ryansouzas/gamecatalog/GamecatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryansouzas.gamecatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamecatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
